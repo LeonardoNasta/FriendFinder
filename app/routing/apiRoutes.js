5ed7b40b97443f8037a31d7dd288886653b62c57
unknown <byleonardonasta@gmail.com>
2019-05-23 08:58:42 -0700

    materialize-fields-stopped-working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,46 +4,45 @@ var path = require("path");
 var friendResults = require("../data/friends.js");
 
 module.exports = function(app) {
+
   app.get("/api/friends", function(req, res) {
     // Returns all friends!
-    res.json(friends);
+    res.json(friendResults);
   });
 
   app.post("/api/friends", function(req, res) {
-
     // get user details
-    var user = req.body;
+    var friendResults = req.body;
     var userResponses = userInput.scores;
-		// console.log('userResponses = ' + userResponses);
+   
 
-		// Compute best friend match
-		var matchName = '';
-		var matchImage = '';
-		var totalDifference = 20000;
 
-		// Examine all existing friends in the list
-		for (var i = 0; i < friendResults.length; i++) {
-			// console.log('friendResults = ' + JSON.stringify(friends[i]));
+    // Compute best friend match
+    var matchName = "";
+    var matchImage = "";
+    var totalDifference = 20000;
 
-			// Compute difference for each question
-			var diff = 0;
-			for (var j = 0; j < userResponses.length; j++) {
-				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
-			}
+    for (var i = 0; i < friendResults.length; i++) {
+      // console.log('friendResults = ' + JSON.stringify(friendResults[i]));
 
-			// If lowest difference, record the friend match
-			if (diff < totalDifference) {
+      // get difference for each question
+      var diff = 0;
+      for (var j = 0; j < userResponses.length; j++) {
+        diff += Math.abs(friendResults[i].scores[j] - userResponses[j]);
+      }
 
-				totalDifference = diff;
-				matchName = friendResults[i].name;
-				matchImage = friendResults[i].photo;
-			}
-		}
+      // If lowest difference, record the friend match
+      if (diff < totalDifference) {
+        totalDifference = diff;
+        matchName = friendResults[i].name;
+        matchImage = friendResults[i].photo;
+      }
+    }
 
-		// Add new user
-		friendResults.push(userInput);
+    // Add new user
+    friendResults.push(userInput);
 
-		// Send appropriate response
-		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
-	});
+    // Send appropriate response
+    res.json({ status: "OK", matchName: matchName, matchImage: matchImage });
+  });
 };
